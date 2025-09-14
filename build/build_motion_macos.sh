b3b18ae3b099fff864b2c02e778c847ee5be7d6b
Mikel <33097309+M1K31@users.noreply.github.com>
2025-09-14 14:40:51 -0400

    Update build_motion_macos.sh
    Signed-off-by: Mikel <33097309+M1K31@users.noreply.github.com>

--- build/build_motion_macos.sh
+++ build/build_motion_macos.sh
@@ -9,9 +9,14 @@ set -e
 echo "--- Installing dependencies with Homebrew ---"
 brew install ffmpeg pkg-config libjpeg libmicrohttpd automake gettext
 
-echo "--- Exporting required PATH for gettext ---"
-# This is required for the build system to find the gettext tools
-export PATH="/usr/local/opt/gettext/bin:$PATH"
+# Replace lines 12-14 with:
+# Create secure temporary directory
+TEMP_DIR=$(mktemp -d)
+trap 'rm -rf "$TEMP_DIR"' EXIT
+
+# Clone into secure temp directory
+git clone https://github.com/Motion-Project/motion.git "$TEMP_DIR/motion-src"
+cd "$TEMP_DIR/motion-src"
 
 echo "--- Cloning motion source code ---"
 # Clone into a temporary directory
